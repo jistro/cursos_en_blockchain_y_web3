9cb1975a3915cedb95e44c0bf5948e4370b1a46a
Kevin <jistrogames@gmail.com>
2023-03-21 21:54:42 -0600

    avance 21 marzo

--- Curso 1/Proyectos de curso/04 Disneyland/disney.sol
+++ Curso 1/Proyectos de curso/04 Disneyland/disney.sol
@@ -4,6 +4,8 @@ pragma experimental ABIEncoderV2;
 import "./ERC20.sol";
 
 contract Disney {
+//---------------------------Declaraciones Iniciales--------------------------------------//
+
     // instancia del contrato token
     ERC20Disney private token;
 
@@ -11,8 +13,57 @@ contract Disney {
     address payable public owner;
 
     // constructor
-    constructor() {
+    constructor() public{
         token = new ERC20Disney(1000);
         owner = payable(msg.sender);
     }
+
+    // estructura de datos para almacenar a los clientes de Disney
+    struct cliente {
+        uint tokensComprados;
+        string [] atraccionesDisfrutadas;
+    }
+
+    // mapping para el registro de clientes
+    mapping (address => cliente) public Clientes;
+//----------------------------------------------------------------------------------------//
+
+//---------------------------------Gestion de tokens--------------------------------------//
+
+    // funcion para establecer precio de un token 
+    function PrecioTokens(uint _numTokens) internal pure returns(uint) {
+        // convercion 1:1 de tokens <=> eth
+        return _numTokens * (1 ether);
+    }
+
+    // compra de tokens para uso de atracciones
+    function compraTokens(uint _numTokens) public payable {
+        // Establecer precio de tokens 
+        uint coste = PrecioTokens(_numTokens);
+
+        // se evalua el dinero que el cliente paga por los tokens
+        require(msg.value >= coste, "No se puede comprar esta cantidad, compra menos o usa mas ETH");
+
+        // diferencia de lo que el cliente paga
+        uint returnValue = msg.value - coste;
+
+        // disney retorna la cantidad de ETH al cliente
+        msg.sender.transfer(returnValue);
+
+        // obtencion del num. de tokens disponible
+        uint Balance = token.balanceOf(owner);
+        require(_numTokens <= Balance, "Lo sentimos, no hay disponible mas de la cantidad que pidio");
+
+        // se transfiere el num. de tokens solicitados al cliente
+        token.transfer(msg.sender, _numTokens);
+
+        // registero de tokens comprados
+        Clientes[msg.sender].tokensComprados = _numTokens;
+
+    }
+
+//----------------------------------------------------------------------------------------//
+
+
+
 }
