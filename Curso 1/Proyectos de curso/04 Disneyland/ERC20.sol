4d66de8baa15d6d249b2bd1b62610f206ec80aa0
Kevin <kevin.padilla.islas@proton.me>
2023-03-30 14:27:17 -0600

    fin Loteria

--- Curso 1/Proyectos de curso/04 Disneyland/ERC20.sol
+++ Curso 1/Proyectos de curso/04 Disneyland/ERC20.sol
@@ -5,19 +5,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Disney is ERC20 {
-
+    address public _owner;
 
     constructor(uint256 initialSupply) ERC20("DisneyDollars", "DYD")  {
         _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
+        _owner = msg.sender; //establecemos el propietario del contrato
     }
     
     function decimals() public view virtual override returns (uint8) {
         return 2; //definimos que seran 2 decimales
     }
 
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
     }
 
-    
+    //transfiere al propietario del contrato
+    function transferToOwner(uint amount) public {
+        _transfer(msg.sender, _owner, amount);
+    }
 }
+
