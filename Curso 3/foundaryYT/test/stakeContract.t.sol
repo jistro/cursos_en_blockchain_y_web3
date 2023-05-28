a4122986f35682950c4ec9af4b4826ffdfeb347d
Kevin <kevin.padilla.islas@proton.me>
2023-05-28 01:07:51 -0600

    avance 17 mayo

---
+++ Curso 3/foundaryYT/test/stakeContract.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/stakeContract.sol";
+import "test/mocks/MockERC20.SOL";
+import "test/utils/Cheats.sol";
+
+contract ContractTest is Test {
+    Cheats internal constant cheats = Cheats(HEVM_ADDRESS);
+    StakeContract public stakeContract;
+    MockERC20 public mockToken;
+
+    function setUp() public {
+        stakeContract = new StakeContract();
+        mockToken = new MockERC20();
+    }
+
+    function testExample(uint8 amount)  public {
+        mockToken.approve(address(stakeContract), amount);
+        bool stakePassed = stakeContract.stake(address(mockToken), amount);
+        cheats.roll(55);
+        assertTrue(stakePassed);
+    }
+}
