9e60cf9948e8ce035591686d2a8c0e0cd99c4307
Kevin <jistro@skiff.com>
2023-06-03 23:02:54 -0600

    avance 3 jun

---
+++ Curso 5/parte01/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export default function NavBar() {
+    return (
+        <>
+            <h1>NavBar</h1>
+        </>
+    )
+}
