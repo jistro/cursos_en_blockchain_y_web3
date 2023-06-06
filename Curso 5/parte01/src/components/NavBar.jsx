1df99af9ea405d03fcd55a3b5076a64b017bd907
Kevin <jistro@skiff.com>
2023-06-05 18:57:07 -0600

    parte 1 terminada

--- Curso 5/parte01/src/components/NavBar.jsx
+++ Curso 5/parte01/src/components/NavBar.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 export default function NavBar() {
     return (
-        <>
-            <h1>NavBar</h1>
-        </>
+        <nav>
+            <img 
+                src="./src/img/eth-diamond-rainbow.svg"
+                alt="Ethereum logo"
+                className='nav--logo'
+            />
+            <h3 className='nav--h3'>Web3</h3>
+            <h4 className='nav--h4'>Curso de react - Proyecto 1</h4>
+        </nav>
     )
 }
