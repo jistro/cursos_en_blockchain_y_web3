503316aaa619fcf7ef4d8f7e3069502735d507ef
Kevin Padilla <jistro@skiff.com>
2023-07-06 16:25:38 -0600

    avance 06/07 COMMIT DE CURSO

--- Curso 6/Leccion 7/foundry-fund-me-23/script/Interactions.s.sol
+++ Curso 6/Leccion 7/foundry-fund-me-23/script/Interactions.s.sol
@@ -6,9 +6,8 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.01 ether;
-    function fundFundMe(address mostRecentlyDeployed) public {
-
+    uint256 constant SEND_VALUE = 1 ether;
+    function fund_FundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
         vm.stopBroadcast();
@@ -17,9 +16,19 @@ contract FundFundMe is Script {
     }
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        fundFundMe(mostRecentlyDeployed);
+        fund_FundMe(mostRecentlyDeployed);
     }
 }
 
 contract WithdrawFundMe is Script {
+    function withdraw_FundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        withdraw_FundMe(mostRecentlyDeployed);
+    }
 }
--- Curso 6/Leccion 7/foundry-fund-me-23/test/integrations/IntegrationTest.t.sol
+++ Curso 6/Leccion 7/foundry-fund-me-23/test/integrations/IntegrationTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
 import {DEPLOY_FundMe} from "../../script/DEPLOYFundMe.s.sol";
-import {FundFundMe} from "../../script/interactions.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 contract IntegrationTest is Test {
     FundMe fundMe;
@@ -13,15 +13,17 @@ contract IntegrationTest is Test {
     function setUp() external{
         DEPLOY_FundMe deployFundMe = new DEPLOY_FundMe();
         fundMe = deployFundMe.run();
-        vm.deal(USER, 100 ether);
+        
     }
 
-    function testUserCanFund() public {
+    function testUserCanFundInteractions() public {
         FundFundMe fundFundMe = new FundFundMe();
-        fundFundMe.fundFundMe(address(fundMe));
+        fundFundMe.fund_FundMe(address(fundMe));
 
-        address funder = fundMe.getFunder(0);
-        assert(funder == USER);
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdraw_FundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
         
     }
 }
